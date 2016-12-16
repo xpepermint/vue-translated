6e902c6ad304235a5eb30eb574d2fd9f7c111803
xpeper <xpepermint@gmail.com>
2016-12-17 00:02:18 +0100

    adding example

---
+++ example/src/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    Examples:
+    <p>{{ $formatMessage('hello', {name: 'John'}) }}</p>
+    <p>{{ $formatNumber(1000.13, {format: 'integer'}) }}</p>
+    <p>{{ $formatDate(Date.now(), {format: 'long'}) }}</p>
+    <p>{{ $formatTime(Date.now(), {format: 'medium'}) }}</p>
+    <p>{{ $formatRelativeTime(Date.now() - 10000) }}</p>
+  </div>
+</template>
---
+++ example/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './app.vue';
+import i18n from './i18n';
+
+new Vue({
+  i18n,
+  el: '#app',
+  render: h => h(App)
+});
